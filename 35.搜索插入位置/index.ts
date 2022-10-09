4e25a7189feae367e2f4bf6d843603716196fc32
Yotsugi <56288345+NonYotsugi@users.noreply.github.com>
2022-10-09 16:42:39 +0800

    day3

--- 35.搜索插入位置/index.ts
+++ 35.搜索插入位置/index.ts
@@ -1,3 +1,19 @@
-function searchInsert(nums: number[], target: number):number {
-  
+function searchInsert(nums, target) {
+	let n = nums.length;
+	let left = 0;
+	let right = n - 1;
+	while(left <= right) {
+		let mid = Math.floor((left + right) / 2);
+		if(nums[mid] > target) {
+			right = mid - 1;
+		} else if(nums[mid] < target) {
+			left = mid + 1;
+		} else {
+			return mid;
+		}
+	}
+	return left;
 }
+
+const result = searchInsert([1,3,5,6], 5);
+console.log('resulte',result)
